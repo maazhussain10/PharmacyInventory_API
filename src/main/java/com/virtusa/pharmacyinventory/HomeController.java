32c4897d14ab80a1c6e292b20d51dca5d81a2ad9
Ashok Kumar A <ashokkumar071001@gmail.com>
2022-03-01 20:03:20 +0530

    Added components

--- src/main/java/com/virtusa/pharmacyinventory/HomeController.java
+++ src/main/java/com/virtusa/pharmacyinventory/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // @CrossOrigin
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/tablet")
     public String home() {
         return "Hello Welcome to Pharmacy Inventory System";
     }
